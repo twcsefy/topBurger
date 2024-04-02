695c616efa7d9aa0ba43ebc1849c207d9417f305
twcsefy <mariasefora87@gmail.com>
2024-04-02 15:42:32 -0300

    02/04dnv

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -18,6 +18,22 @@ class ProdutoController extends Controller
                 'imagem'=>asset('storage/' .$produto->imagem),
             ];
         });
+
         return response()->json($produtosComImagem);
+        }
+    
+        public function store(Request $request){
+
+            $produtoData = $request->all();
+
+            if ($request->hasFile('imagem')){
+                $imagem = $request->file('imagem');
+                $nomeImagem = time().'.'.$imagem->getClientOriginalExtension();
+                $caminhoImagem = $imagem->storeAs('images/produtos', $nomeImagem, 'public');
+                $produtoData['imagem'] = $caminhoImagem;
+            }
+            $produto = Produto::create($produtoData);
+            return response()->json(['produto'=>$produto], 201);
+
         }
 }
